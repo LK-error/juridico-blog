da4090a4254e11aaa1da47505d0f5ed96d6258d7
Gabriel Gomes <gabi.dallacqua05@gmail.com>
2025-09-30 10:25:33 -0300

    blog finalizado

--- intranet_flores/settings.py
+++ intranet_flores/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=4gwgn$7**msy8zd%88pwrcppcyr2b#ja2n8!zusgk&osmwil@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [blogflores.pythonanywhere.com]
+ALLOWED_HOSTS = ['lucasfloresadv.pythonanywhere.com', 'www.lucasfloresadv.pythonanywhere.com']
 
 
 # Application definition
